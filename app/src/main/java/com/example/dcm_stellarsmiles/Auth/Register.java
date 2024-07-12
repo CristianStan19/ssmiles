7310bb33185f14c4153daecc72778ee24560ac71
cristi <stancristian2002@gmail.com>
2024-07-12 12:09:46 +0300

    xd

--- app/src/main/java/com/example/dcm_stellarsmiles/Auth/Register.java
+++ app/src/main/java/com/example/dcm_stellarsmiles/Auth/Register.java
@@ -9,7 +9,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -29,12 +28,13 @@ import com.example.dcm_stellarsmiles.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.Calendar;
 
@@ -56,7 +56,10 @@ public class Register extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+        // Initialize Firebase Auth and Firestore
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance(); // Initialize Firestore here
+
         etEmail = findViewById(R.id.etEmailLogin);
         etPw = findViewById(R.id.etPwLogin);
         loginNow = findViewById(R.id.loginNow);
@@ -76,84 +79,143 @@ public class Register extends AppCompatActivity {
             endIconDrawable.setColorFilter(ContextCompat.getColor(this, R.color.black), PorterDuff.Mode.SRC_IN);
         }
 
-        loginNow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LogIn.class);
-                startActivity(intent);
-                finish();
-            }
+        loginNow.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), LogIn.class);
+            startActivity(intent);
+            finish();
         });
 
-        etBirthDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog();
-            }
-        });
+        etBirthDate.setOnClickListener(v -> showDatePickerDialog());
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email, password, phoneNumber, birthDate, CNP, fullName;
-                boolean isSmoker, isDrinker;
-                email = etEmail.getText().toString();
-                password = etPw.getText().toString();
-                phoneNumber = etPhoneNumber.getText().toString();
-                birthDate = etBirthDate.getText().toString();
-                CNP = etCNP.getText().toString();
-                fullName = etFullName.getText().toString();
-                RadioButton selectedSmokeButton = findViewById(smokeRadioGroup.getCheckedRadioButtonId());
+        btnRegister.setOnClickListener(v -> {
+            String email, password, phoneNumber, birthDate, CNP, fullName;
+            boolean isSmoker, isDrinker;
+            email = etEmail.getText().toString();
+            password = etPw.getText().toString();
+            phoneNumber = etPhoneNumber.getText().toString();
+            birthDate = etBirthDate.getText().toString();
+            CNP = etCNP.getText().toString();
+            fullName = etFullName.getText().toString();
+
+            RadioButton selectedSmokeButton = findViewById(smokeRadioGroup.getCheckedRadioButtonId());
+            RadioButton selectedDrinkButton = findViewById(drinkRadioGroup.getCheckedRadioButtonId());
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(getApplicationContext(), "Enter a valid email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(phoneNumber) || phoneNumber.length() != 10) {
+                Toast.makeText(getApplicationContext(), "Enter a valid phone number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(fullName) || !Character.isUpperCase(fullName.charAt(0)) || fullName.length() < 3) {
+                Toast.makeText(getApplicationContext(), "Full name must start with a capital letter and be at least 3 characters long", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (selectedSmokeButton == null) {
+                Toast.makeText(getApplicationContext(), "Please select your smoking status", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
                 isSmoker = selectedSmokeButton.isChecked();
-                RadioButton selectedDrinkButton = findViewById(drinkRadioGroup.getCheckedRadioButtonId());
-                isDrinker = selectedDrinkButton.isChecked();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(Register.this, "Account created successfully.",
-                                            Toast.LENGTH_SHORT).show();
-                                    FirebaseUser user = task.getResult().getUser();
-                                    String userID = user.getUid();
-                                    db = FirebaseFirestore.getInstance();
-                                    Customer customer = new Customer(fullName, email, phoneNumber, birthDate, CNP, isSmoker, isDrinker);
-                                    customer.setCustomerID(userID);
-                                    Intent intent = new Intent(getApplicationContext(), LogIn.class);
-                                    startActivity(intent);
-                                    finish();
-
-                                    db.collection("customers").document(userID).set(customer)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    // Handle success
-                                                }
-                                            }).addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    // Handle failure
-                                                }
-                                            });
-
-                                } else {
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(Register.this, "Account creation failure.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
             }
+
+            if (selectedDrinkButton == null) {
+                Toast.makeText(getApplicationContext(), "Please select your drinking status", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                isDrinker = selectedDrinkButton.isChecked();
+            }
+
+            // Check if CNP is unique
+            db.collection("customers")
+                    .whereEqualTo("cnp", CNP)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            boolean cnpExists = false;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                cnpExists = true;
+                                break;
+                            }
+
+                            if (cnpExists) {
+                                Toast.makeText(getApplicationContext(), "CNP already exists", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Check if phoneNumber is unique
+                                db.collection("customers")
+                                        .whereEqualTo("phoneNumber", phoneNumber)
+                                        .get()
+                                        .addOnCompleteListener(phoneTask -> {
+                                            if (phoneTask.isSuccessful()) {
+                                                boolean phoneNumberExists = false;
+                                                for (QueryDocumentSnapshot document : phoneTask.getResult()) {
+                                                    phoneNumberExists = true;
+                                                    break;
+                                                }
+
+                                                if (phoneNumberExists) {
+                                                    Toast.makeText(getApplicationContext(), "Phone number already exists", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    // Proceed with registration logic
+                                                    mAuth.createUserWithEmailAndPassword(email, password)
+                                                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                                                    if (task.isSuccessful()) {
+                                                                        Toast.makeText(Register.this, "Account created successfully.",
+                                                                                Toast.LENGTH_SHORT).show();
+                                                                        FirebaseUser user = task.getResult().getUser();
+                                                                        String userID = user.getUid();
+                                                                        Customer customer = new Customer(fullName, email, phoneNumber, birthDate, CNP, isSmoker, isDrinker);
+                                                                        customer.setCustomerID(userID);
+                                                                        Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                                                                        startActivity(intent);
+                                                                        finish();
+
+                                                                        db.collection("customers").document(userID).set(customer)
+                                                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                    @Override
+                                                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                                                        // Handle success
+                                                                                    }
+                                                                                }).addOnFailureListener(new OnFailureListener() {
+                                                                                    @Override
+                                                                                    public void onFailure(@NonNull Exception e) {
+                                                                                        // Handle failure
+                                                                                    }
+                                                                                });
+
+                                                                    } else {
+                                                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                                                        Toast.makeText(Register.this, "Account creation failure.",
+                                                                                Toast.LENGTH_SHORT).show();
+                                                                    }
+                                                                }
+                                                            });
+                                                }
+                                            } else {
+                                                Toast.makeText(getApplicationContext(), "Error checking phone number", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error checking CNP", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
